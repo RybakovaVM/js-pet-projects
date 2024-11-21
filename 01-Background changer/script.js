d4802c61c28a3e6636f71345ddef667474cdbf1f
RybakovaVM <valerikr.3005@gmail.com>
2024-11-21 23:18:01 +0500

    first commit

---
+++ 01-Background changer/script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+button.addEventListener('click', () =>{
+    let hexColor = generateHex()
+    document.body.style.backgroundColor = hexColor
+    // console.log(getRandomNumber());
+    color.textContent = hexColor
+    color.style.color = hexColor
+    
+})
+function generateHex(){    //Генерирует такой: #F42A451
+    let hexColor = '#'
+    for( let i = 0; i<6; i++){
+        hexColor += hex[getRandomNumber()]
+    }
+    return hexColor 
+}
+
+
+function getRandomNumber() {       //Генерирует символ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+    return Math.floor(Math.random()* hex.length)
+}
+generateHex()
